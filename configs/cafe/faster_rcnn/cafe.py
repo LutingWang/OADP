ddc3df6efba8ecae2cbc204ab6d2c802682d7247
LutingWang <wangluting.wlt@alibaba-inc.com>
2022-10-09 19:13:56 +0800

    feat: cafe dyhead nodh

--- configs/cafe/faster_rcnn/cafe.py
+++ configs/cafe/faster_rcnn/cafe.py
@@ -1,6 +1,6 @@
 _base_ = [
-    # '../_base_/models/faster_rcnn_r50_fpn.py',
-    '../_base_/models/dh_faster_rcnn_r50_fpn.py',
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    # '../_base_/models/dh_faster_rcnn_r50_fpn.py',
 
     '../_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py',
