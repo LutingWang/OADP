709ef0184e2f4981612f9db84d0710172384c3db
dingzihan02 <dingzihan02@meituan.com>
2022-10-17 13:45:02 +0800

    feat: 4gpu train

--- configs/cafe/_base_/datasets/coco_48_17.py
+++ configs/cafe/_base_/datasets/coco_48_17.py
@@ -1,6 +1,8 @@
 dataset_type = 'CocoDataset4817'
 data_root = 'data/coco/'
 data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json.COCO_48_17.48',
