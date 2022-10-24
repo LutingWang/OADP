374ff27fd13928a06954307569ec9874b7fcecb9
LutingWang <wangluting.wlt@alibaba-inc.com>
2022-10-24 15:52:02 +0800

    refact: separate patch 48 17 config

--- configs/cafe/faster_rcnn/cafe_48_17.py
+++ configs/cafe/faster_rcnn/cafe_48_17.py
@@ -16,6 +16,8 @@ _base_ = [
     # 'mixins/multilabel_48_17.py',
     # 'mixins/post.py',
 
+    'mixins/patch_48_17.py',
+
 ]
 
 model = dict(
@@ -34,47 +36,11 @@ model = dict(
             norm_cfg=dict(type='SyncBN', requires_grad=True),
             # norm_cfg=None,
         ),
-
-#         patch_head=dict(
-#             type='Shared2FCBBoxHead',
-#             with_reg=False,
-#             cls_predictor_cfg=dict(
-#                 type='Classifier',
-#                 pretrained='data/coco/prompt/prompt1.pth',
-#                 split='COCO_48_17',
-#                 num_base_classes=48,
-#             ),
-#             loss=dict(
-#                 type='AsymmetricLoss',
-#                 weight=dict(
-#                     type='WarmupScheduler',
-#                     value=16,
-#                     iter_=1000,
-#                 ),
-#                 gamma_neg=4,
-#                 gamma_pos=0,
-#                 clip=0.05,
-#                 disable_torch_grad_focal_loss=True,
-#             ),
-#         ),
-
     ),
     distiller=dict(
         student_hooks=dict(),
         adapts=dict(),
-        losses=dict(
-#             loss_clip_patches=dict(
-#                 type='L1Loss',
-#                 norm=True,
-#                 fields=['patch_feats', 'clip_patch_feats'],
-#                 weight=dict(
-#                     type='WarmupScheduler',
-#                     value=128,
-#                     iter_=200,
-#                 ),
-#                 # reduction='mean',
-#             ),
-        ),
+        losses=dict(),
     ),
     test_cfg=dict(
         rcnn=dict(
