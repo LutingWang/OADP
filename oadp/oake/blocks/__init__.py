b21707d2714384dd53aa4390f06a1142de44e05f
Noietch <yangheqing@buaa.edu.cn>
2025-01-14 14:27:34 +0800

    rename

--- oadp/oake/blocks/__init__.py
+++ oadp/oake/blocks/__init__.py
@@ -1,6 +1,3 @@
 from .coco import *
 from .datasets import *
-from .lvis import *
-from .objects365 import *
 from .runners import *
-from .v3det import *
--- oadp/oake/objects/__init__.py
+++ oadp/oake/objects/__init__.py
@@ -1,5 +1,3 @@
 from .coco import *
 from .datasets import *
-from .objects365 import *
 from .runners import *
-from .v3det import *
