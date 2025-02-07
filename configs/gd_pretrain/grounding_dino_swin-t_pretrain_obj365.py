2b4ebf256887324004a6891346bb293438734ef3
weiziyu <weiziyu@meituan.com>
2025-02-07 11:41:17 +0800

    bert path

--- configs/gd_pretrain/grounding_dino_swin-t_pretrain_obj365.py
+++ configs/gd_pretrain/grounding_dino_swin-t_pretrain_obj365.py
@@ -5,7 +5,7 @@ _base_ = [
 server_root = '/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtcv/weiziyu/109/OADP'
 
 pretrained = server_root + 'ckpt/swin_tiny_patch4_window7_224.pth'
-lang_model_name = "pretrained/google-bert/bert-base-uncased"
+lang_model_name = "/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtcv/weiziyu/Checkpoints/bert-base-uncased"
 
 model = dict(
     type='GroundingDINO',
